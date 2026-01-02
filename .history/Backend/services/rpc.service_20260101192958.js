d47a6c1acd6a10548b78bda679b719877845ffff
vishal shitole <vishal.shitole.2004@gmail.com>
2026-01-02 21:05:12 +0530

    deleted .history folder

--- .history/Backend/services/rpc.service_20260101192958.js
+++
@@ -1,5 +0,0 @@
-import { ethers } from "ethers";
-import dotenv from "dotenv";
-dotenv.config();
-
-export const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
--- .history/Backend/src/server_20260101130311.js
+++
@@ -1,5 +0,0 @@
-import app from "../app.js";
-
-app.listen(4000, () => {
-  console.log("Backend running on http://localhost:4000");
-});
